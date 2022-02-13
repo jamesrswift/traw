fd07013cec849095c069cd57d5bfac5853bf2817
James Swift <james.rt.swift@gmail.com>
2022-02-13 21:06:18 +0000

    More Endpoints

---
+++ src/tier1/api/v1/me/trophies.ts
@@ -0,0 +1,25 @@
+import authenticated_requestor from "../../../authenticated_requestor";
+
+export interface trophies_response{
+    kind: string,
+    data: {
+        trophies: {
+            kind: string,
+            data: {
+                icon_70: string,
+                granted_at: number | null,
+                url: string | null,
+                icon_40: string,
+                name: string,
+                award_id: string | null,
+                id: string | null,
+                description: string | null
+            }
+        }[]
+    }
+}
+
+export async function karma( requestor: authenticated_requestor ) : Promise<trophies_response>{
+    const result = await requestor.get({url: '/api/v1/me/trophies'})
+    return <trophies_response>result.data
+}
---
+++ src/tier1/prefs/friends.ts
@@ -0,0 +1,18 @@
+import authenticated_requestor from "../authenticated_requestor";
+
+export interface friends_response{
+    kind: string,
+    data: {
+        children: {
+            date: number,
+            rel_id: string,
+            name: string,
+            id: string
+        }[]
+    }
+}
+
+export async function friends( requestor: authenticated_requestor ) : Promise<friends_response[]>{
+    const result = await requestor.get({url: '/prefs/friends'})
+    return <friends_response[]>result.data
+}
